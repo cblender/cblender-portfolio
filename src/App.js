44cdc80871be7bec1069af3390d40a00c93cc31a
cblender <christopher.blender@gmail.com>
2020-10-28 19:37:47 -0400

    POST REBUILD - basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app_Header flex_column">
+        <div className="header_text flex_column">
+          <h1>cblender</h1>
+          <h2>web design portfolio</h2>
+        </div>
+        <div className="body_article">
+          <div className="article_content flex_row">
+            <div className="content_text">
+              <h3>Description</h3>
+            </div>
+          </div>
+        </div>
       </header>
+      <div className="app_Background">
+        <div className="background_image"></div>
+      </div>
+      <div className="app_Body flex_column">
+        <div className="body_container">
+          {/*  */}
+          {/* ARTICLE ONE */}
+          <div className="body_article flex_column">
+            <div className="article_head">
+              <div className="article_banner"></div>
+              <div className="article_title">
+                <h2>Article Title</h2>
+              </div>
+            </div>
+            <div className="article_content flex_column">
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_image">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
+          {/* ARTICLE TWO */}
+          <div className="body_article flex_column">
+            <div className="article_head">
+              <div className="article_banner"></div>
+              <div className="article_title">
+                <h2>Article Title</h2>
+              </div>
+            </div>
+            <div className="article_content flex_column">
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_image">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+            </div>
+          </div>
+
+          {/*  */}
+          {/* ARTICLE THREE */}
+          <div className="body_article flex_column">
+            <div className="article_head">
+              <div className="article_banner"></div>
+              <div className="article_title">
+                <h2>Article Title</h2>
+              </div>
+            </div>
+            <div className="article_content flex_column">
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_image">
+                <h3>TEXT</h3>
+              </div>
+              <div className="content_text">
+                <h3>TEXT</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
